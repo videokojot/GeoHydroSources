68919cd5da4e004616335b2578c9b6d56d47191e
ntb\jindra <jindrich.cincibuch@gmail.com>
2019-03-12 21:42:03 +0100

    initial commit with todos.

---
+++ GeoHydro/GeoHydroCore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OPTANO.Modeling.Optimization;
+using OPTANO.Modeling.Optimization.Solver.GLPK;
+
+namespace GeoHydroCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var model = new Model();
+
+            var solver = new GLPKSolver();
+
+
+            // TODOs:
+            // define model
+            // normalize markers
+            // read config
+
+
+            // model:
+            // weights sum should be 1
+            // weights should be >= 0 <= 1
+            // indicator variables (whether source is used)
+            // count of used sources (if defined)
+            // equation for each marker
+            // min: diff between target and resulting mix
+        }
+    }
+}
